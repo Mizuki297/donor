d855786e83eca752de23a1ddec74a09a5ef68fed
Mizuki297 <thanaphat.20297@gmail.com>
2019-08-28 19:49:34 +0700

    update url

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.View;
+
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -14,10 +16,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
+
 public class MainActivity extends AppCompatActivity {
 
-    List<String> arrList = new ArrayList<>();
-    TextView txtView;
+    private String url = "https://glyphographic-runwa.000webhostapp.com/api/get_post_hpt.php";
+
+    public List<String> arrList = new ArrayList<String>();
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spin = (Spinner) findViewById(R.id.spinner_search_HPT);
-        arrList.add("สถานที่รักษา");
-        //เพื่มข้อมูลลงอาเรย์
+            arrList.add("กรุณาเลือกสถานที่รักษา");
+            arrList.add("โรงพยาบาลปทุมเวช");
+            arrList.add("โรงพยาบาลสัตว์ไอเวท");
+
         ArrayAdapter<String> arrAD = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,arrList);
         spin.setAdapter(arrAD);
+
     }
+
     public void onClick_button_add (View view){
         Button btn_next = (Button)findViewById(R.id.button_add);
         Intent intent = new Intent();
---
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+public class Post {
+
+    private int htp_id;
+    private String htp_name, htp_location;
+
+    public int getHtp_id(){
+        return htp_id;
+    }
+    public void setHtp_id(int htp_id){
+        this.htp_id = htp_id;
+    }
+    public String getHtp_name(){
+        return htp_name;
+    }
+    public void setHtp_name(String htp_name){
+        this.htp_name = htp_name;
+    }
+    public String getHtp_location(){
+        return htp_location;
+    }
+    public void setHtp_location(String htp_location){
+        this.htp_location = htp_location;
+    }
+}
