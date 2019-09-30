bbf2935e10084ba4c9bba175a4c010bdfec640b0
Mizuki297 <thanaphat.20297@gmail.com>
2019-09-30 20:37:07 +0700

    marster

--- app/src/main/java/com/example/myapplication/PHPServiceAPI.java
+++ app/src/main/java/com/example/myapplication/PHPServiceAPI.java
@@ -53,7 +53,7 @@ public interface PHPServiceAPI {
     @FormUrlEncoded
     @POST("api/select_Cat.php")
     Call<List<CatModel>> getcat_list(
-            @Field("HPT_id") String user_id,
+            @Field("HPT_id") String HPT_id,
             @Field("blood_type") String blood_type
     );
 
